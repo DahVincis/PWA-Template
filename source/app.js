57df9fe2021e359969df8c3afc63facf4d34db72
DahVincis <ph.leao2099@gmail.com>
2023-12-16 14:17:59 -0500

    modified:   public/index.html 	modified:   source/app.js

--- source/app.js
+++ source/app.js
@@ -9,22 +9,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.get('/info', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/info.html'));
+    res.sendFile(path.join(__dirname, '../public/info.html'));
 });
 
 app.get('/schedule', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/schedule.html'));
+    res.sendFile(path.join(__dirname, '../public/schedule.html'));
 });
 
 // Additional routes for your server-side logic can go here
 // You can require and use modules from your '/source/server-side' directory
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
