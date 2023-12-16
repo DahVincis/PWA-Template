d5ab967f756d58ab63e18839cd96cab032272fc8
DahVincis <ph.leao2099@gmail.com>
2023-12-16 15:18:15 -0500

    modified:   app.js 	modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/public/index.html

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
 // Serve static files from React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, './client/public')));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname, './client/public/index.html'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
